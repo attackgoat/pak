08ca7676bf0e5c333828866e6dc9300e6a29a809
John Wells <john@attackgoat.com>
2023-08-26 20:58:53 -0400

    Improve index buffer API

--- src/index.rs
+++ src/index.rs
@@ -1,18 +1,11 @@
 use serde::{de::DeserializeOwned, Deserialize, Serialize};
 
-#[derive(Clone, Copy, Debug, Deserialize, Serialize)]
-enum Index {
-    U8,
-    U16,
-    U32,
-}
-
 #[derive(Clone, Debug, Deserialize, Serialize)]
 pub struct IndexBuffer {
     #[serde(with = "serde_bytes")]
     buf: Vec<u8>,
 
-    ty: Index,
+    ty: IndexType,
 }
 
 impl IndexBuffer {
@@ -30,7 +23,10 @@ impl IndexBuffer {
                 buf.push(idx as u8);
             }
 
-            Self { buf, ty: Index::U8 }
+            Self {
+                buf,
+                ty: IndexType::U8,
+            }
         } else if max_vertex <= u16::MAX as _ {
             let mut buf = Vec::with_capacity(indices.len() << 1);
             for &idx in indices {
@@ -39,7 +35,7 @@ impl IndexBuffer {
 
             Self {
                 buf,
-                ty: Index::U16,
+                ty: IndexType::U16,
             }
         } else {
             let mut buf = Vec::with_capacity(indices.len() << 2);
@@ -49,15 +45,42 @@ impl IndexBuffer {
 
             Self {
                 buf,
-                ty: Index::U32,
+                ty: IndexType::U32,
             }
         }
     }
 
-    pub fn index_buffer(&self) -> Vec<u32> {
+    pub fn as_u8(&self) -> Option<Vec<u8>> {
         match self.ty {
-            Index::U8 => self.buf.iter().copied().map(|idx| idx as _).collect(),
-            Index::U16 => {
+            IndexType::U8 => Some(self.buf.iter().copied().map(|idx| idx as _).collect()),
+            _ => None,
+        }
+    }
+
+    pub fn as_u16(&self) -> Option<Vec<u16>> {
+        match self.ty {
+            IndexType::U8 => Some(self.buf.iter().copied().map(|idx| idx as u16).collect()),
+            IndexType::U16 => {
+                debug_assert_eq!(self.buf.len() % 2, 0);
+
+                let count = self.buf.len() >> 1;
+                let mut res = Vec::with_capacity(count);
+                for idx in 0..count {
+                    let idx = idx << 1;
+                    let data = &self.buf[idx..idx + 2];
+                    res.push(u16::from_ne_bytes([data[0], data[1]]));
+                }
+
+                Some(res)
+            }
+            _ => None,
+        }
+    }
+
+    pub fn as_u32(&self) -> Vec<u32> {
+        match self.ty {
+            IndexType::U8 => self.buf.iter().copied().map(|idx| idx as _).collect(),
+            IndexType::U16 => {
                 debug_assert_eq!(self.buf.len() % 2, 0);
 
                 let count = self.buf.len() >> 1;
@@ -70,7 +93,7 @@ impl IndexBuffer {
 
                 res
             }
-            Index::U32 => {
+            IndexType::U32 => {
                 debug_assert_eq!(self.buf.len() % 4, 0);
 
                 let count = self.buf.len() >> 2;
@@ -88,10 +111,14 @@ impl IndexBuffer {
 
     pub fn index_count(&self) -> usize {
         match self.ty {
-            Index::U8 => self.buf.len(),
-            Index::U16 => self.buf.len() >> 1,
-            Index::U32 => self.buf.len() >> 2,
+            IndexType::U8 => self.buf.len(),
+            IndexType::U16 => self.buf.len() >> 1,
+            IndexType::U32 => self.buf.len() >> 2,
         }
+    }
+
+    pub fn index_type(&self) -> IndexType {
+        self.ty
     }
 
     pub fn triangle_count(&self) -> usize {
@@ -99,18 +126,40 @@ impl IndexBuffer {
     }
 }
 
+#[derive(Clone, Copy, Debug, Deserialize, Eq, PartialEq, Serialize)]
+pub enum IndexType {
+    U8,
+    U16,
+    U32,
+}
+
 #[cfg(test)]
 mod tests {
-    use crate::index::IndexBuffer;
+    use crate::index::{IndexBuffer, IndexType};
 
     #[test]
     fn index_buffer_u8() {
-        let buf = IndexBuffer::new(&[0, 1, 2]);
+        let index_buf = IndexBuffer::new(&[0, 1, 2]);
 
-        assert_eq!(buf.triangle_count(), 1);
-        assert_eq!(buf.index_count(), 3);
+        assert_eq!(index_buf.triangle_count(), 1);
+        assert_eq!(index_buf.index_count(), 3);
+        assert_eq!(index_buf.index_type(), IndexType::U8);
 
-        let buf = buf.index_buffer();
+        let buf = index_buf.as_u8().unwrap();
+
+        assert_eq!(buf.len(), 3);
+        assert_eq!(buf[0], 0);
+        assert_eq!(buf[1], 1);
+        assert_eq!(buf[2], 2);
+
+        let buf = index_buf.as_u16().unwrap();
+
+        assert_eq!(buf.len(), 3);
+        assert_eq!(buf[0], 0);
+        assert_eq!(buf[1], 1);
+        assert_eq!(buf[2], 2);
+
+        let buf = index_buf.as_u32();
 
         assert_eq!(buf.len(), 3);
         assert_eq!(buf[0], 0);
@@ -120,12 +169,21 @@ mod tests {
 
     #[test]
     fn index_buffer_u16() {
-        let buf = IndexBuffer::new(&[0, 1, 42_000]);
+        let index_buf = IndexBuffer::new(&[0, 1, 42_000]);
 
-        assert_eq!(buf.triangle_count(), 1);
-        assert_eq!(buf.index_count(), 3);
+        assert_eq!(index_buf.triangle_count(), 1);
+        assert_eq!(index_buf.index_count(), 3);
+        assert_eq!(index_buf.index_type(), IndexType::U16);
+        assert_eq!(index_buf.as_u8(), None);
 
-        let buf = buf.index_buffer();
+        let buf = index_buf.as_u16().unwrap();
+
+        assert_eq!(buf.len(), 3);
+        assert_eq!(buf[0], 0);
+        assert_eq!(buf[1], 1);
+        assert_eq!(buf[2], 42_000);
+
+        let buf = index_buf.as_u32();
 
         assert_eq!(buf.len(), 3);
         assert_eq!(buf[0], 0);
@@ -135,12 +193,15 @@ mod tests {
 
     #[test]
     fn index_buffer_u32() {
-        let buf = IndexBuffer::new(&[0, 1, 100_000]);
+        let index_buf = IndexBuffer::new(&[0, 1, 100_000]);
 
-        assert_eq!(buf.triangle_count(), 1);
-        assert_eq!(buf.index_count(), 3);
+        assert_eq!(index_buf.triangle_count(), 1);
+        assert_eq!(index_buf.index_count(), 3);
+        assert_eq!(index_buf.index_type(), IndexType::U32);
+        assert_eq!(index_buf.as_u8(), None);
+        assert_eq!(index_buf.as_u16(), None);
 
-        let buf = buf.index_buffer();
+        let buf = index_buf.as_u32();
 
         assert_eq!(buf.len(), 3);
         assert_eq!(buf[0], 0);
