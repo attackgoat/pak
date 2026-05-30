eaff3c9f0a762c02dc84fcfedb2f2122cb5ab828
John Wells <john@attackgoat.com>
2026-05-30 10:53:44 -0400

    Make Texture channels selectable

--- src/mesh.rs
+++ src/mesh.rs
@@ -134,7 +134,7 @@ bitflags! {
         const NORMAL = Self::POSITION.bits() | (1 << 2);
         const TANGENT = Self::POSITION.bits() | (1 << 3);
         const TEXTURE0 = Self::POSITION.bits() | (1 << 4);
-        const TEXTURE1 = Self::TEXTURE0.bits() | (1 << 5);
+        const TEXTURE1 = Self::POSITION.bits() | (1 << 5);
     }
 }
 
